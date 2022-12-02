17a31c920fe89d00ab9ab28a4dd379fb5665dab2
JungInBaek <44565979+JungInBaek@users.noreply.github.com>
2022-12-02 16:45:22 +0900

    controller, dao, mybatis-config 설정
    CartController 생성
    CartDAO 생성
    mybatis-config cartVO, cartProductVO 설정

--- src/main/java/com/multi/fineapple/dao/CartDAO.java
+++ src/main/java/com/multi/fineapple/dao/CartDAO.java
@@ -1,5 +1,12 @@
 package com.multi.fineapple.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class CartDAO {
 
+	@Autowired
+	private SqlSessionTemplate my;
 }
