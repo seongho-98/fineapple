1e539a1a55ce3788fdc8a185009b77d5f96cb608
hyeonyeong <hyeonyeong@test.com>
2022-12-02 11:09:57 +0900

    CategoryVO.java 추가

---
+++ src/main/java/com/multi/fineapple/vo/CategoryVO.java
@@ -0,0 +1,25 @@
+package com.multi.fineapple.vo;
+
+public class CategoryVO {
+	int category_id;
+	String category_name;
+	
+	public int getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
+	}
+	public String getCategory_name() {
+		return category_name;
+	}
+	public void setCategory_name(String category_name) {
+		this.category_name = category_name;
+	}
+	
+	@Override
+	public String toString() {
+		return "CategoryVO [category_id=" + category_id + ", category_name=" + category_name + "]";
+	}
+	
+}
